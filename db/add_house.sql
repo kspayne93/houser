009656966baf99a58663663178a2eda467d823a3
kspayne93 <kyle@investavante.com>
2019-01-04 15:26:18 -0700

    added image, mortgage, & rent params to be taken in and added to table

--- db/add_house.sql
+++ db/add_house.sql
@@ -1,4 +1,4 @@
-INSERT INTO houses(property_name, address, city, state, zip)
-VALUES ($(property_name), $(address), $(city), $(state), $(zip));
+INSERT INTO houses(property_name, address, city, state, zip, image, mortgage, rent)
+VALUES ($(property_name), $(address), $(city), $(state), $(zip), $(image), $(mortgage), $(rent));
 
 select * from houses;
