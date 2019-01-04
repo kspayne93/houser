1e6b7b28e231a31632e7ccc242d4f3be7375921b
kspayne93 <kyle@investavante.com>
2019-01-04 15:30:56 -0700

    moved app.listen to be inside massive (db connects first, then listens)

--- server/server.js
+++ server/server.js
@@ -9,8 +9,11 @@ app.use(express.json()); //Built in Middleware, turning request from JSON back i
 const {CONNECTION_STRING, SERVER_PORT} = process.env;
 
 massive(CONNECTION_STRING).then((dbInstance) => {
-   app.set('db', dbInstance) //the value of 'db' is dbInstance
-   console.log('db connected')
+   app.set('db', dbInstance) //the value of 'db' here is dbInstance. 
+   console.log('connected to database')
+   app.listen(SERVER_PORT, () => console.log(`listening on port ${SERVER_PORT}`));
+   // proxy should be set to run on the same port that nodemon is running on. 
+   //"proxy": "http://localhost:4000" 
 })
 
 
@@ -22,12 +25,3 @@ app.post(`/api/inventory`, controller.createItem)
 app.delete(`/api/inventory/:id`, controller.deleteItem)
 
 
-
-
-
-
-
-
-app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} ducks marching on rome`));
-// proxy should be set to run on the same port that nodemon is running on. 
-//"proxy": "http://localhost:4000"
