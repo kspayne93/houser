6314c72a59abdf7a43e855db6d689e6ba8f29237
kspayne93 <kyle@investavante.com>
2019-01-04 15:27:30 -0700

    added image, mortgage, and rent columns to table

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,4 @@
+-- Create Table
 create table houses(
     id serial PRIMARY KEY,
     property_name varchar(30),
@@ -5,14 +6,25 @@ create table houses(
     city varchar(100),
     state varchar(2),
     zip integer
-)
+);
 
+-- Adding dummy data
+insert into houses(property_name, address, city, state, zip)
+values('Kyles House', '123 Main St', 'Spanish Fork', 'UT', 84660);
 
 insert into houses(property_name, address, city, state, zip)
-values('Kyles House', '123 Main St', 'Spanish Fork', 'UT', 84660)
+values('Milos House', '456 Center St', 'Salt Lake City', 'UT', 85858);
 
 insert into houses(property_name, address, city, state, zip)
-values('Milos House', '456 Center St', 'Salt Lake City', 'UT', 85858)
+values('Marisas House', '789 East St', 'Los Angeles', 'CA', 90210);
 
-insert into houses(property_name, address, city, state, zip)
-values('Marisas House', '789 East St', 'Los Angeles', 'CA', 90210)
+
+-- Adding Columns
+ALTER TABLE houses
+  ADD image VARCHAR(200);
+
+ALTER TABLE houses
+  ADD mortgage integer;
+
+ALTER TABLE houses
+  ADD rent integer;
