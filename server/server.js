5b7fc4d5c430a5f1e42be03b5f896bc7910145d5
kspayne93 <kyle@investavante.com>
2019-01-10 17:33:37 -0700

    added notes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-const express = require('express');
+const express = require('express'); //express is a back-end server framework 
 const controller = require('./controller');
-const massive = require('massive');
+const massive = require('massive'); //massive is how we connect to our database and use SQL commands
 
 const app = express();
-app.use(express.json()); //Built in Middleware, turning request from JSON back into the function so it can be parsed. Axios prefers to send data as JSON, but it needs to be converted back before it reaches the endpoint.
+app.use(express.json()); //express.json is built in middleware, turning request from JSON back into the function so it can be parsed. Axios prefers to send data as JSON, but it needs to be converted back before it reaches the endpoint.
 
-const {CONNECTION_STRING, SERVER_PORT} = process.env;
+const {CONNECTION_STRING, SERVER_PORT} = process.env; //deconstructing from the .env file at the root
 
 massive(CONNECTION_STRING).then((dbInstance) => {
    app.set('db', dbInstance) //the value of 'db' here is dbInstance. 
